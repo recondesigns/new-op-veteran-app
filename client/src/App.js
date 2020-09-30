97452543b6340aa3323f021f39010977e100ca1a
Michael Stedman <michaelstedman81@gmail.com>
2020-09-29 20:58:35 -0600

    Commit
    Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react'
+import styled from 'styled-components'
+import { AppStateContext } from './providers/Store'
 
-function App() {
+const AppContainer = styled.div`
+  border: 1px solid dodgerblue;
+`
+
+export default function App() {
+  const [appState] = useContext(AppStateContext)
+  console.log(appState)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppContainer>
+      <h1>App Component</h1>
+    </AppContainer>
+  )
 }
-
-export default App;
---
+++ client/src/providers/Store.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+export const AppStateContext = React.createContext()
+
+export default function Store({ children }) {
+    const [appState, setAppState] = useState({
+        userLoggedIn: false,
+        isLoading: false,
+        currentUser: {
+            userDetails: {
+                fistName: '',
+                lastName: '',
+                userPhone: '',
+                isAdmin: false
+            },
+            vendorDetails: {
+                vendorName: '',
+                vendorDescription: '',
+                vendorAddress: '',
+                vendorAptSuite: '',
+                vendorCity: '',
+                vendorState: '',
+                vendorZipcode: '',
+                sponsorshipLevel: ''
+            },
+            eventDetails: {
+                registrationComplete: false,
+                boothObj: '',
+                powered: false
+            }
+        }
+    })
+
+    return (
+        <AppStateContext.Provider value={[appState, setAppState]}>
+            {children}
+        </AppStateContext.Provider>
+    )
+}
